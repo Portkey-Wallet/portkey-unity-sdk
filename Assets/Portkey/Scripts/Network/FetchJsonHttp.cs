c93122dd88bb139c70d0dcf28fd073af226a1cf7
zhifenglee <lee@portkey.finance>
2023-06-14 12:32:02 +0800

    test(core - http added unit test for http calls for FetchJsonHttp):

--- Assets/Portkey/Scripts/Network/FetchJsonHttp.cs
+++ Assets/Portkey/Scripts/Network/FetchJsonHttp.cs
@@ -12,48 +12,86 @@ namespace Portkey.Network
     {
         public IEnumerator Get<T>(string url, IHttp.successCallback<T> successCallback, IHttp.errorCallback errorCallback)
         {
-            UnityWebRequest request = UnityWebRequest.Get(url); ;
-
-            yield return request.SendWebRequest();
-
-            if(request.error != null)
+            using (UnityWebRequest request = UnityWebRequest.Get(url))
             {
-                errorCallback(request.error);
-                yield break;
+                yield return request.SendWebRequest();
+
+                if (request.error != null)
+                {
+                    errorCallback(request.error);
+                    yield break;
+                }
+                else if (request.responseCode != 200)
+                {
+                    errorCallback(request.responseCode.ToString());
+                    yield break;
+                }
+
+                T ret;
+                try
+                {
+                    ret = JsonConvert.DeserializeObject<T>(request.downloadHandler.text);
+                }
+                catch (Exception e)
+                {
+                    errorCallback(e.Message);
+                    throw;
+                }
+                successCallback(ret);
+
+                request.Dispose();
             }
-            else if(request.responseCode != 200)
-            {
-                errorCallback(request.responseCode.ToString());
-                yield break;
-            }
-            
-            T ret = JsonConvert.DeserializeObject<T>(request.downloadHandler.text);
-            successCallback(ret);
         }
 
         public IEnumerator Post<T>(string url, string body, IHttp.successCallback<T> successCallback, IHttp.errorCallback errorCallback)
         {
             string jsonData = JsonConvert.SerializeObject(new{query = body});
             byte[] postData = Encoding.ASCII.GetBytes(jsonData);
-            UnityWebRequest request = UnityWebRequest.Post(url, UnityWebRequest.kHttpVerbPOST);
-            request.uploadHandler = new UploadHandlerRaw(postData);
-            request.SetRequestHeader("Content-Type", "application/json");
-            
-            yield return request.SendWebRequest();
-
-            if(request.error != null)
+            using (UnityWebRequest request = new UnityWebRequest(url, UnityWebRequest.kHttpVerbPOST))
             {
-                errorCallback(request.error);
-                yield break;
-            }
-            else if(request.responseCode != 200)
-            {
-                errorCallback(request.responseCode.ToString());
-                yield break;
-            }
+                request.uploadHandler = new UploadHandlerRaw(postData);
+                request.disposeUploadHandlerOnDispose = true;
+                
+                request.downloadHandler = new DownloadHandlerBuffer();
+                request.disposeDownloadHandlerOnDispose = true;
+                
+                request.SetRequestHeader("Content-Type", "application/json");
 
-            T ret = JsonConvert.DeserializeObject<T>(request.downloadHandler.text);
-            successCallback(ret);
+                yield return request.SendWebRequest();
+
+                if (request.error != null)
+                {
+                    errorCallback(request.error);
+                    request.uploadHandler.Dispose();
+                    request.downloadHandler.Dispose();
+                    request.Dispose();
+                    yield break;
+                }
+                else if (request.result != UnityWebRequest.Result.Success)
+                {
+                    errorCallback(request.responseCode.ToString());
+                    request.uploadHandler.Dispose();
+                    request.downloadHandler.Dispose();
+                    request.Dispose();
+                    yield break;
+                }
+
+                T ret;
+                try
+                {
+                    ret = JsonConvert.DeserializeObject<T>(request.downloadHandler.text);
+                }
+                catch (Exception e)
+                {
+                    errorCallback(e.Message);
+                    throw;
+                }
+                successCallback(ret);
+
+                request.uploadHandler.Dispose();
+                request.downloadHandler.Dispose();
+                request.Dispose();
+            }
         }
         
         

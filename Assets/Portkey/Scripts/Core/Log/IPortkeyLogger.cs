d7db166be7eb5e1c1c474987b86f59521d031326
zhifenglee <lee@portkey.finance>
2023-06-20 14:29:41 +0800

    feat(core - log added interface and access Debugger class for better abstraction):

--- Assets/Portkey/Scripts/Core/Log/IPortkeyLogger.cs
+++ Assets/Portkey/Scripts/Core/Log/IPortkeyLogger.cs
@@ -1,46 +1,29 @@
-using System;
-
 namespace Portkey.Core
 {
-
     /// <summary>
-    /// Portkey debugger class for logging.
+    /// Interface for logging.
     /// </summary>
-    public static class Debugger
+    public interface IPortkeyLogger
     {
-        public static IPortkeyLogger Logger;
-
         /// <summary>
         /// Logs a message.
         /// </summary>
         /// <param name="message">The message to log.</param>
-        public static void Log(object message)
-        {
-            Logger?.Log(message);
-        }
+        void Log(object message);
         /// <summary>
         /// Logs a warning message.
         /// </summary>
         /// <param name="message">The message to log.</param>
-        public static void LogWarning(object message)
-        {
-            Logger?.LogWarning(message);
-        }
+        void LogWarning(object message);
         /// <summary>
         /// Logs an error message.
         /// </summary>
         /// <param name="message">The message to log.</param>
-        public static void LogError(object message)
-        {
-            Logger?.LogError(message);
-        }
+        void LogError(object message);
         /// <summary>
         /// Logs an exception.
         /// </summary>
         /// <param name="exception">The exception to log.</param>
-        public static void LogException(Exception exception)
-        {
-            Logger?.LogException(exception);
-        }
+        void LogException(System.Exception exception);
     }
 }

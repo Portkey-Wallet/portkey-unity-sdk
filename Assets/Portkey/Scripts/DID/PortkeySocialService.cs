fe6a44edfdd12e038903692f905ab9eb5753e8e0
zhifenglee <lee@portkey.finance>
2023-06-26 17:41:04 +0800

    feat(did - services): added basic portkey config

--- Assets/Portkey/Scripts/DID/PortkeySocialService.cs
+++ Assets/Portkey/Scripts/DID/PortkeySocialService.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using Portkey.Core;
 using Portkey.Network;
+using Unity.Plastic.Newtonsoft.Json;
+using Unity.Plastic.Newtonsoft.Json.Linq;
 
 namespace Portkey.DID
 {
@@ -8,8 +10,10 @@ namespace Portkey.DID
     //this class will be used to make requests to the Portkey Social API
     public class PortkeySocialService : ServiceBase<IHttp>, IPortkeySocialService
     {
-        public PortkeySocialService(IHttp http) : base(http)
+        private PortkeyConfig _config;
+        public PortkeySocialService(IHttp http, PortkeyConfig config) : base(http)
         {
+            this._config = config;
         }
 
         public IEnumerator GetVerificationCode(SendVerificationCodeRequestParams requestParams, IVerificationService.SuccessCallback<SendVerificationCodeResult> successCallback,
@@ -97,16 +101,64 @@ namespace Portkey.DID
             IPortkeySocialService.ErrorCallback errorCallback)
         {
             throw new System.NotImplementedException();
+            /*
+            string url = _config.apiBaseUrl + "/api/app/account/isGoogleRecaptchaOpen";
+            return this.Http.Post(url, JsonToObject<bool>(successCallback, errorCallback),
+                (error) =>
+                {
+                    errorCallback(error);
+                });*/
         }
 
         public IEnumerator GetPhoneCountryCode(IPortkeySocialService.SuccessCallback<ICountryItem[]> successCallback, IPortkeySocialService.ErrorCallback errorCallback)
         {
-            throw new System.NotImplementedException();
+            string url = _config.apiBaseUrl + "/api/app/phone/info";
+            return this.Http.Get(url, JsonToObject<ICountryItem[]>(successCallback, errorCallback),
+                (error) =>
+                {
+                    errorCallback(error);
+                });
         }
 
         public IEnumerator GetPhoneCountryCodeWithLocal(IPortkeySocialService.SuccessCallback<IPhoneCountryCodeResult> successCallback, IPortkeySocialService.ErrorCallback errorCallback)
         {
-            throw new System.NotImplementedException();
+            string url = _config.apiBaseUrl + "/api/app/phone/info";
+            return this.Http.Get(url, JsonToObject<IPhoneCountryCodeResult>(successCallback, errorCallback),
+                (error) =>
+                {
+                    errorCallback(error);
+                });
+        }
+        
+        private static IHttp.successCallback JsonToObject<T>(IPortkeySocialService.SuccessCallback<T> successCallback, IPortkeySocialService.ErrorCallback errorCallback)
+        {
+            return (response) =>
+            {
+                var json = JObject.Parse(response);
+                string data = null;
+                if (json.TryGetValue("errors", out var errorMessage))
+                {
+                    //process data and wrapper
+                    var error = errorMessage.ToString();
+                    errorCallback(error);
+                    return;
+                }
+                                    
+                if (json.TryGetValue("data", out var value))
+                {
+                    //process data and wrapper
+                    data = value.ToString();
+                }
+                else
+                {
+                    errorCallback("No data in response. Incorrect response format.");
+                    return;
+                }
+                //deserialize response
+                var deserializedObject = JsonConvert.DeserializeObject<T>(data);
+                //call success callback
+                successCallback(deserializedObject);
+            };
         }
     }
 }

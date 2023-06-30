911879d6f5439ed382b5523d6ee70c399973385d
zhifenglee <lee@portkey.finance>
2023-06-30 13:54:24 +0800

    refactor(singleton-coroutine): a class to allow non-Monobehaviour classes to run coroutines

---
+++ Assets/Portkey/Scripts/Utitlies/StaticCoroutine.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Portkey.Utilities
+{
+    /// <summary>
+    /// A static helper class for running coroutines.
+    /// This allows classes that are not MonoBehaviours to run coroutines,
+    /// thereby allowing abstract class to be inherited without it being a MonoBehaviour.
+    /// </summary>
+    public static class StaticCoroutine
+    {
+        private class CoroutineHolder : MonoBehaviour { }
+
+        private static CoroutineHolder _runner;
+        private static CoroutineHolder Runner
+        {
+            get
+            {
+                if (_runner == null)
+                {
+                    _runner = new GameObject("Portkey Static Coroutine").AddComponent<CoroutineHolder>();
+                    Object.DontDestroyOnLoad(_runner);
+                }
+                return _runner;
+            }
+        }
+
+        /// <summary>
+        /// Start running a coroutine.
+        /// </summary>
+        /// <param name="coroutine">The coroutine to run.</param>
+        /// <returns>Coroutine returned from running StartCoroutine.</returns>
+        public static Coroutine StartCoroutine(IEnumerator coroutine)
+        {
+            return Runner.StartCoroutine(coroutine);
+        }
+    }
+}

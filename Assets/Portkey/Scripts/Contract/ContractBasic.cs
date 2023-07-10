958236c4578457b1c3d2fa9b95beb9d777c22a8a
zhifenglee-portkey <zhifeng.lee@aelf.io>
2023-07-10 22:28:56 +0800

    feat(did - contract): made changes to ContractBasic based on new wrapper interface for IChain

--- Assets/Portkey/Scripts/Contract/ContractBasic.cs
+++ Assets/Portkey/Scripts/Contract/ContractBasic.cs
@@ -23,9 +23,9 @@ namespace Portkey.Contract
             try
             {
                 //var chainInfo = _chainOptions.ChainInfos[chainId];
-                var baseUrl = "http://192.168.66.61:8000";
+                //var baseUrl = "http://192.168.66.61:8000";
                 var privateKey = "83829798ac92d428ed13b29fe60ace1a7a10e7a347bdc9f23a85615339068f1c";
-                var contractAddress = "2imqjpkCwnvYzfnr61Lp2XQVN2JU17LPkA9AZzmRZzV5LRRWmR";
+                //var contractAddress = "2imqjpkCwnvYzfnr61Lp2XQVN2JU17LPkA9AZzmRZzV5LRRWmR";
 
                 //var client = new AElfClient(baseUrl);
                 //var ownAddress = client.GetAddressFromPrivateKey(privateKey);
@@ -33,11 +33,11 @@ namespace Portkey.Contract
                 //var ownAddress = "045ab0516fda4eeb504ad8d7ce8a2a24e5af6004afa9ff3ee26f2c697e334be48c31597e1905711a6aa749fc475787000b5d6260bcf0d457f23c60aa60bb6c8602";
                 
                 var transaction =
-                    await _chain.Client.GenerateTransactionAsync(ownAddress, ContractAddress,
+                    await _chain.GenerateTransactionAsync(ownAddress, ContractAddress,
                         methodName, param);
-                var txWithSign = _chain.Client.SignTransaction(privateKey, transaction);
+                var txWithSign = _chain.SignTransaction(privateKey, transaction);
 
-                var result = await _chain.Client.ExecuteTransactionAsync(new ExecuteTransactionDto
+                var result = await _chain.ExecuteTransactionAsync(new ExecuteTransactionDto
                 {
                     RawTransaction = txWithSign.ToByteArray().ToHex()
                 });

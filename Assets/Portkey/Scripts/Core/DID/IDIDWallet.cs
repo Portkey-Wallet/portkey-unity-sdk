55a23f7265d685d258a690c59e76247b94ad97ec
zhifenglee-portkey <zhifeng.lee@aelf.io>
2023-07-14 18:02:15 +0800

    feat(did - wallet): added interface and implementation

---
+++ Assets/Portkey/Scripts/Core/DID/IDIDWallet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+
+namespace Portkey.Core
+{
+    public interface IDIDWallet : IWallet, IDIDAccountMethods
+    {
+        /// <summary>
+        /// For adding a manager account to the DID.
+        /// </summary>
+        /// <param name="editManagerParams">Parameters for adding manager account.</param>
+        public IEnumerator AddManager(EditManagerParams editManagerParams, IHttp.successCallback successCallback, ErrorCallback errorCallback);
+        /// <summary>
+        /// For removing a manager account to the DID.
+        /// </summary>
+        /// <param name="editManagerParams">Parameters for removing manager account.</param>
+        public IEnumerator RemoveManager(EditManagerParams editManagerParams, IHttp.successCallback successCallback, ErrorCallback errorCallback);
+    }
+}

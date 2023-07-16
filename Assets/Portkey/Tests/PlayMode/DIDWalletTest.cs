8a4b9aa55012b4a199d1f4705158e96ed85fc4e9
zhifenglee-portkey <zhifeng.lee@aelf.io>
2023-07-16 14:22:52 +0800

    test(did - wallet): updated test case

--- Assets/Portkey/Tests/PlayMode/DIDWalletTest.cs
+++ Assets/Portkey/Tests/PlayMode/DIDWalletTest.cs
@@ -1,9 +1,12 @@
 using System.Collections;
+using System.Collections.Generic;
+using Moq;
 using NUnit.Framework;
 using Portkey.Core;
 using Portkey.DID;
 using Portkey.Network;
 using Portkey.Storage;
+using Portkey.Utilities;
 using UnityEngine.TestTools;
 
 namespace Portkey.Test
@@ -12,13 +15,88 @@ namespace Portkey.Test
     {
         private IPortkeySocialService socialService = new PortkeySocialServiceMock();
         private IStorageSuite<string> storageSuite = new NonPersistentStorageMock<string>("");
-        private IAccountProvider<WalletAccount> accountProvider = new AccountProvider();
         private IConnectService connectService = new ConnectService<RequestHttp>("", new RequestHttp());
+        
+        private static Mock<IAccountProvider<WalletAccount>> GetAccountProviderMock()
+        {
+            var accountProviderMock = new Mock<IAccountProvider<WalletAccount>>();
+            accountProviderMock.Setup(provider => provider.CreateAccount())
+                .Returns(new WalletAccount(
+                    new BlockchainWallet("address_mock", 
+                        "privateKey_mock", 
+                        "mnemonic_mock",
+                        "publicKey_mock")));
+            return accountProviderMock;
+        }
+        
+        private static Mock<IPortkeySocialService> GetSocialServiceMock()
+        {
+            var socialServiceMock = new Mock<IPortkeySocialService>();
+            socialServiceMock.Setup(service => service.GetRegisterStatus(It.IsAny<string>(), It.IsAny<QueryOptions>(),
+                    It.IsAny<SuccessCallback<RegisterStatusResult>>(), It.IsAny<ErrorCallback>()))
+                .Returns((string sessionId, QueryOptions queryOptions, SuccessCallback<RegisterStatusResult> successCallback,
+                    ErrorCallback errorCallback) =>
+                {
+                    successCallback(new RegisterStatusResult
+                    {
+                        caAddress = "caAddress_mock",
+                        caHash = "caHash_mock",
+                        registerStatus = "pass"
+                    });
 
+                    return new List<string>().GetEnumerator();
+                });
+            socialServiceMock.Setup(service => service.Register(It.IsAny<RegisterParams>(),
+                    It.IsAny<SuccessCallback<SessionIdResult>>(), It.IsAny<ErrorCallback>()))
+                .Returns((RegisterParams registerParams, SuccessCallback<SessionIdResult> successCallback,
+                    ErrorCallback errorCallback) =>
+                {
+                    successCallback(new SessionIdResult
+                    {
+                        sessionId = "sessionId_mock"
+                    });
+
+                    return new List<string>().GetEnumerator();
+                });
+            socialServiceMock.Setup(service => service.GetHolderInfo(It.IsAny<GetHolderInfoParams>(),
+                    It.IsAny<SuccessCallback<IHolderInfo>>(), It.IsAny<ErrorCallback>()))
+                .Returns((GetHolderInfoParams requestParams, SuccessCallback<IHolderInfo> successCallback,
+                    ErrorCallback errorCallback) =>
+                {
+                    successCallback(new IHolderInfo
+                    {
+                        caAddress = "caAddress_mock",
+                        caHash = "caHash_mock",
+                        guardianList = new GuardianList {
+                            guardians = new Guardian[]
+                            {
+                                new Guardian
+                                {
+                                    guardianIdentifier = "guardianIdentifier_mock",
+                                }
+                            }
+                        },
+                        managerInfos = new Manager[]
+                        {
+                            new Manager
+                            {
+                                address = "address_mock",
+                            }
+                        }
+                    });
+
+                    return new List<string>().GetEnumerator();
+                });
+            return socialServiceMock;
+        }
+        
         [UnityTest]
         public IEnumerator GetRegisterStatusTest()
         {
-            var didWallet = new DIDWallet<WalletAccount>(socialService, storageSuite, accountProvider, connectService);
+            var accountProviderMock = GetAccountProviderMock();
+            var socialServiceMock = GetSocialServiceMock();
+
+            var didWallet = new DIDWallet<WalletAccount>(socialServiceMock.Object, storageSuite, accountProviderMock.Object, connectService);
             
             return didWallet.GetRegisterStatus("AELF_mock", "sessionId_mock", (response) =>
             {
@@ -29,11 +107,14 @@ namespace Portkey.Test
                 Assert.Fail(error);
             });
         }
-        
+
         [UnityTest]
         public IEnumerator RegisterTest()
         {
-            var didWallet = new DIDWallet<WalletAccount>(socialService, storageSuite, accountProvider, connectService);
+            var accountProviderMock = GetAccountProviderMock();
+            var socialServiceMock = GetSocialServiceMock();
+
+            var didWallet = new DIDWallet<WalletAccount>(socialServiceMock.Object, storageSuite, accountProviderMock.Object, connectService);
             var registerParam = new RegisterParams
             {
                 type = AccountType.Google,
@@ -42,9 +123,46 @@ namespace Portkey.Test
             
             return didWallet.Register(registerParam, (result) =>
             {
+                accountProviderMock.Verify(provider => provider.CreateAccount(), Times.AtMostOnce());
+                socialServiceMock.Verify(service => service.Register(It.IsAny<RegisterParams>(), It.IsAny<SuccessCallback<SessionIdResult>>(), It.IsAny<ErrorCallback>()), Times.AtMostOnce());
+                socialServiceMock.Verify(service => service.GetRegisterStatus(It.IsAny<string>(), It.IsAny<QueryOptions>(), It.IsAny<SuccessCallback<RegisterStatusResult>>(), It.IsAny<ErrorCallback>()), Times.AtMostOnce());
+                socialServiceMock.Verify(service => service.GetHolderInfo(It.IsAny<GetHolderInfoParams>(), It.IsAny<SuccessCallback<IHolderInfo>>(), It.IsAny<ErrorCallback>()), Times.AtMostOnce());
                 Assert.AreEqual(result.Status.caAddress, "caAddress_mock");
                 Assert.AreEqual(result.Status.caHash, "caHash_mock");
                 Assert.AreEqual(result.SessionId, "sessionId_mock");
+            }, error =>
+            {
+                Assert.Fail(error);
+            });
+        }
+        
+        [UnityTest]
+        public IEnumerator RegisterTwiceTest()
+        {
+            var accountProviderMock = GetAccountProviderMock();
+            var socialServiceMock = GetSocialServiceMock();
+
+            var didWallet = new DIDWallet<WalletAccount>(socialServiceMock.Object, storageSuite, accountProviderMock.Object, connectService);
+            var registerParam = new RegisterParams
+            {
+                type = AccountType.Google,
+                chainId = "AELF_mock"
+            };
+            
+            return didWallet.Register(registerParam, (result) =>
+            {
+                StaticCoroutine.StartCoroutine(didWallet.Register(registerParam, (result) =>
+                {
+                    Assert.Fail("Should not be here");
+                }, error =>
+                {
+                    accountProviderMock.Verify(provider => provider.CreateAccount(), Times.AtMostOnce());
+                    socialServiceMock.Verify(service => service.Register(It.IsAny<RegisterParams>(), It.IsAny<SuccessCallback<SessionIdResult>>(), It.IsAny<ErrorCallback>()), Times.AtMostOnce());
+                    socialServiceMock.Verify(service => service.GetRegisterStatus(It.IsAny<string>(), It.IsAny<QueryOptions>(), It.IsAny<SuccessCallback<RegisterStatusResult>>(), It.IsAny<ErrorCallback>()), Times.AtMostOnce());
+                    socialServiceMock.Verify(service => service.GetHolderInfo(It.IsAny<GetHolderInfoParams>(), It.IsAny<SuccessCallback<IHolderInfo>>(), It.IsAny<ErrorCallback>()), Times.AtMostOnce());
+                    
+                    Assert.AreEqual(error, "Account already logged in.");
+                }));
             }, error =>
             {
                 Assert.Fail(error);

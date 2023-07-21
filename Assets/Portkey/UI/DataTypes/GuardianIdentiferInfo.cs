ab23da5e314a247d5fad84518e8e91d07a94aaab
zhifenglee-portkey <zhifeng.lee@aelf.io>
2023-07-21 17:15:11 +0800

    refactor(ui - signup): brought class out to its own file

---
+++ Assets/Portkey/UI/DataTypes/GuardianIdentiferInfo.cs
@@ -0,0 +1,13 @@
+using Portkey.Core;
+
+namespace Portkey.UI
+{
+    public class GuardianIdentifierInfo
+    {
+        public string identifier;
+        public AccountType accountType;
+        public string token;
+        public bool isLoginGuardian;
+        public string chainId;
+    }
+}

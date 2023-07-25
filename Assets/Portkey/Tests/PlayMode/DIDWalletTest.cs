1ac42d7430ce64954f488176c8d19d01afcb6c8b
zhifenglee-portkey <zhifeng.lee@aelf.io>
2023-07-25 11:39:42 +0800

    test(did - wallet): fixed test used wallet

--- Assets/Portkey/Tests/PlayMode/DIDWalletTest.cs
+++ Assets/Portkey/Tests/PlayMode/DIDWalletTest.cs
@@ -8,8 +8,6 @@ using Google.Protobuf;
 using Moq;
 using Newtonsoft.Json;
 using NUnit.Framework;
-using Portkey.Chain;
-using Portkey.Contract;
 using Portkey.Contracts.CA;
 using Portkey.Core;
 using Portkey.DID;
@@ -28,13 +26,13 @@ namespace Portkey.Test
     {
         private IStorageSuite<string> storageSuite = new NonPersistentStorageMock<string>("");
         private IConnectService connectService = new ConnectService<RequestHttp>("", new RequestHttp());
-        
+
         private static readonly BlockchainWallet Wallet = 
-            new BlockchainWallet("TrmPcaqbqmbrztv6iJuN4zeuDmQxvjF5ujbvDDQo9Q1B4ye2T",
-                                "83829798ac92d428ed13b29fe60ace1a7a10e7a347bdc9f23a85615339068f1c",
-                                "mock",
-                                "045ab0516fda4eeb504ad8d7ce8a2a24e5af6004afa9ff3ee26f2c697e334be48c31597e1905711a6aa749fc475787000b5d6260bcf0d457f23c60aa60bb6c8602");
-        
+            new BlockchainWallet("Q9GTUEz6tJVXdFTTxPhg8MZwWQ4LoHpPSPExRsPb2tBxdruqb",
+                            "b1eae0819f3af0189283342c79adcac9028b251da52056e1e5b2ba79a8b4ccf1",
+                            "mock",
+                            "0473eeb8965c3ebabc388055b7a2a8b8f9f4ea0e546055b7207a23a921e9642c5f68a4f0d9e66efba116d3adc018e8bc5eea5f6600b60168ac73e5db4b6cea693a");
+
         private static Mock<IAccountProvider<WalletAccount>> GetAccountProviderMock()
         {
             var accountProviderMock = new Mock<IAccountProvider<WalletAccount>>();
@@ -354,7 +352,7 @@ namespace Portkey.Test
         [UnityTest]
         public IEnumerator GetVerifierServersLiveTest()
         {
-            var accountProviderMock = GetAccountProviderMock();
+            var accountProvider = new AccountProvider();
             var socialServiceMock = GetSocialServiceMock();
             var encryptionMock = GetEncryptionMock();
             
@@ -369,13 +367,13 @@ namespace Portkey.Test
                     return new List<string>().GetEnumerator();
                 });
             
-            var didWallet = new DIDWallet<WalletAccount>(socialServiceMock.Object, storageSuite, accountProviderMock.Object, connectService, contractProviderMock.Object, encryptionMock.Object);
+            var didWallet = new DIDWallet<WalletAccount>(socialServiceMock.Object, storageSuite, accountProvider, connectService, contractProviderMock.Object, encryptionMock.Object);
 
             var done = false;
             yield return didWallet.GetVerifierServers("AELF", (result) =>
             {
                 done = true;
-                accountProviderMock.Verify(provider => provider.CreateAccount(), Times.Once());
+                //accountProviderMock.Verify(provider => provider.CreateAccount(), Times.Once());
                 contractProviderMock.Verify(provider => provider.GetContract(It.IsAny<string>(), It.IsAny<SuccessCallback<IContract>>(), It.IsAny<ErrorCallback>()), Times.Once());
                 Assert.AreNotEqual(null, result);
             }, error =>
@@ -388,7 +386,8 @@ namespace Portkey.Test
             {
                 yield return new WaitForSeconds(1);
             }
-        }*/
+        }
+        */
         
         [UnityTest]
         public IEnumerator GetVerifierServersTest()

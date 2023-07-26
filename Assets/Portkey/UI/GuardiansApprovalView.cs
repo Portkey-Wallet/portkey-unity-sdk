fcd1f3a5e27de3b4cc5f10e456293be9ab563991
zhifenglee-portkey <zhifeng.lee@aelf.io>
2023-07-26 14:35:22 +0800

    feat(ui - signin): implemented creating guardian items based on holder data

--- Assets/Portkey/UI/GuardiansApprovalView.cs
+++ Assets/Portkey/UI/GuardiansApprovalView.cs
@@ -11,6 +11,7 @@ public class GuardiansApprovalView : MonoBehaviour
 {
     [SerializeField] private DID did;
     [SerializeField] private GameObject guardianItemList;
+    [SerializeField] private GameObject guardianItemPrefab;
     
     private GuardianIdentifierInfo _guardianIdentifierInfo;
     private List<UserGuardianStatus> _guardianStatusList = new List<UserGuardianStatus>();
@@ -43,42 +44,80 @@ public class GuardiansApprovalView : MonoBehaviour
             var currentGuardiansList = new List<UserGuardianStatus>();
             foreach (var guardian in guardians)
             {
-                var key = $"{guardian.guardianIdentifier}&{guardian.verifierId}";
-                var identifier = guardian.guardianIdentifier ?? guardian.identifierHash;
-                var verifier = verifierMap[guardian.verifierId];
-                var guardianItem = new GuardianItem
-                {
-                    guardian = guardian,
-                    verifier = verifier,
-                    identifier = identifier,
-                    key = key
-                };
+                var newGuardianStatus = CreateUserGuardianStatus(guardian, guardianStatusMap, approvedMap);
+                newGuardianStatus.guardianItem.verifier = verifierMap[guardian.verifierId];
                 
-                if(IsMatchingAccessTokenInfo(_guardianIdentifierInfo, guardianItem))
-                {
-                    guardianItem.accessToken = _guardianIdentifierInfo.token;
-                }
-
-                var newGuardianStatus = guardianStatusMap.TryGetValue(key, out var guardianStatus) ? guardianStatus : new UserGuardianStatus();
-                if (approvedMap.TryGetValue(key, out var approvedGuardian))
-                {
-                    newGuardianStatus.status = VerifierStatus.Verified;
-                    newGuardianStatus.verificationDoc = approvedGuardian.verificationDoc;
-                    newGuardianStatus.signature = approvedGuardian.signature;
-                }
-
-                newGuardianStatus.guardianItem = guardianItem;
+                newGuardianStatus = UpdateStatusIfApproved(GetGuardianKey(guardian), newGuardianStatus, approvedMap);
+                
                 currentGuardiansList.Add(newGuardianStatus);
             }
 
             _guardianStatusList = currentGuardiansList;
+            
+            ClearGuardianItems();
             CreateGuardianItems(_guardianStatusList);
         }, OnError));
     }
 
-    private static void CreateGuardianItems(List<UserGuardianStatus> guardianStatusList)
+    private void ClearGuardianItems()
     {
-        
+        foreach (Transform child in guardianItemList.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    private UserGuardianStatus CreateUserGuardianStatus(Guardian guardian,
+        Dictionary<string, UserGuardianStatus> guardianStatusMap, Dictionary<string, GuardiansApproved> approvedMap)
+    {
+        var key = GetGuardianKey(guardian);
+        var identifier = guardian.guardianIdentifier ?? guardian.identifierHash;
+        var guardianItem = new GuardianItem
+        {
+            guardian = guardian,
+            identifier = identifier,
+            key = key
+        };
+
+        if (IsMatchingAccessTokenInfo(_guardianIdentifierInfo, guardianItem))
+        {
+            guardianItem.accessToken = _guardianIdentifierInfo.token;
+        }
+
+        var newGuardianStatus = guardianStatusMap.TryGetValue(key, out var guardianStatus)
+            ? guardianStatus
+            : new UserGuardianStatus();
+
+        newGuardianStatus.guardianItem = guardianItem;
+        return newGuardianStatus;
+    }
+
+    private static string GetGuardianKey(Guardian guardian)
+    {
+        return $"{guardian.guardianIdentifier}&{guardian.verifierId}";
+    }
+
+    private static UserGuardianStatus UpdateStatusIfApproved(string key, UserGuardianStatus newGuardianStatus, IReadOnlyDictionary<string, GuardiansApproved> approvedMap)
+    {
+        if (approvedMap.TryGetValue(key, out var approvedGuardian))
+        {
+            newGuardianStatus.status = VerifierStatus.Verified;
+            newGuardianStatus.verificationDoc = approvedGuardian.verificationDoc;
+            newGuardianStatus.signature = approvedGuardian.signature;
+        }
+        return newGuardianStatus;
+    }
+
+    private void CreateGuardianItems(List<UserGuardianStatus> guardianStatusList)
+    {
+        foreach (var userGuardianStatus in guardianStatusList)
+        {
+            var guardianItem = Instantiate(guardianItemPrefab, guardianItemList.transform).GetComponent<GuardianItemComponent>();
+            if(guardianItem != null)
+            {
+                guardianItem.SetUserGuardianStatus(userGuardianStatus);
+            }
+        }
     }
 
     private static bool IsMatchingAccessTokenInfo(GuardianIdentifierInfo guardianIdentifierInfo, GuardianItem baseGuardian)

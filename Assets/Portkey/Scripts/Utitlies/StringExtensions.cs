ff0c6a6ca9697eb84646278804dd1156fc3024a9
zhifenglee-portkey <zhifeng.lee@aelf.io>
2023-08-16 17:30:03 +0800

    refactor(did - connection service): added string extension for address

---
+++ Assets/Portkey/Scripts/Utitlies/StringExtensions.cs
@@ -0,0 +1,15 @@
+using System.Linq;
+using AElf.Types;
+
+namespace Portkey.Utilities
+{
+    public static class StringExtensions
+    {
+        public static string RemoveAllWhiteSpaces(this string str)
+        {
+            return string.Concat(str.Where(c => !char.IsWhiteSpace(c)));
+        }
+        
+        public static Address ToAddress(this string address) => address == null ? null : Address.FromBase58(address);
+    }
+}

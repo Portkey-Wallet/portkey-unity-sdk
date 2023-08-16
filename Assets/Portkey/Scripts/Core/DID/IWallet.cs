052fe012b6c5859ed92a86cb842d19dba8753826
zhifenglee-portkey <zhifeng.lee@aelf.io>
2023-08-16 11:10:41 +0800

    refactor(did - wallet): changed IWallet from abstract to interface

--- Assets/Portkey/Scripts/Core/DID/IWallet.cs
+++ Assets/Portkey/Scripts/Core/DID/IWallet.cs
@@ -1,16 +1,10 @@
-using AElf.Types;
-
 namespace Portkey.Core
 {
-    public abstract class IWallet : ISigner
+    public interface IWallet : ISigner
     {
         protected KeyPair KeyPair { get; set; }
         protected string PrivateKey => KeyPair.PrivateKey;
         public string Address => KeyPair.Address;
         public string PublicKey => KeyPair.PublicKey;
-        
-        public abstract Transaction SignTransaction(Transaction transaction);
-
-        public abstract byte[] Sign(string data);
     }
 }

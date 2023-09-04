7cf74bdb075f84177d927c79c4a76a30361b596e
zhifenglee-portkey <lee@portkey.finance>
2023-09-04 16:14:20 +0800

    feat(verification-view): added controller

---
+++ Assets/Portkey/UI/VerifyCodeViewController.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+namespace Portkey.UI
+{
+    public class VerifyCodeViewController : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI detailsText = null;
+        [SerializeField] private GuardianItemComponent guardianItem = null;
+        
+    }
+}

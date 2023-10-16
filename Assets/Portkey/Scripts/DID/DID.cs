c320c17c4e49394495d5a4eb61d155f4e4235b1b
zhifenglee-portkey <lee@portkey.finance>
2023-10-16 10:59:50 +0800

    docs(did): added documentation

--- Assets/Portkey/Scripts/DID/DID.cs
+++ Assets/Portkey/Scripts/DID/DID.cs
@@ -13,6 +13,9 @@ using UnityEngine;
 
 namespace Portkey.DID
 {
+    /// <summary>
+    /// PortkeySDK is the main class to interact with Portkey.
+    /// </summary>
     public class DID : MonoBehaviour
     {
         [SerializeField] private IHttp _request;
@@ -65,26 +68,70 @@ namespace Portkey.DID
             _didWallet = new DIDAccount(_portkeySocialService, _signingKeyGenerator, _connectService, _caContractProvider, _accountRepository, _accountGenerator, _appLogin, _qrLogin, _browserWalletExtension);
             AuthService = new AuthService(_portkeySocialService, _didWallet, _socialProvider, _socialVerifierProvider, _config, _verifierService);
         }
+        
+        /// <summary>
+        /// AuthService provides functions to login and logout.
+        /// </summary>
         public IAuthService AuthService { get; private set; }
+        
+        /// <summary>
+        /// Chain provider provides IChain objects which corresponds to chain deployed on the blockchain.
+        /// </summary>
         public IChainProvider ChainProvider { get; private set; }
+        
+        /// <summary>
+        /// Biometric feature available on iOS and Android.
+        /// </summary>
         public IBiometric Biometric => _biometricProvider.GetBiometric();
 
+        /// <summary> The Save function saves the current logged in DID wallet to a file.        
+        /// &lt;para&gt;@param password The password used to encrypt the wallet.&lt;/para&gt;
+        /// &lt;para&gt;@param keyName The name of the key that will be saved in this wallet.&lt;/para&gt;</summary>
+        ///
+        /// <param name="string password"> The password used to encrypt the wallet.</param>
+        /// <param name="string keyName"> ///     the name of the key to be saved. 
+        /// </param>
+        ///
+        /// <returns> A boolean value that indicates whether the operation was successful.</returns>
         public bool Save(string password, string keyName)
         {
             return _didWallet.Save(password, keyName);
         }
 
+        /// <summary> The Load function loads a DID wallet from the file system.        
+        /// &lt;para&gt;@param password The password used to decrypt the DID wallet.&lt;/para&gt;
+        /// &lt;para&gt;@param keyName The name of the key to be loaded.&lt;/para&gt;</summary>
+        ///
+        /// <param name="string password"> The password used to encrypt the wallet</param>
+        /// <param name="string keyName"> /// the name of the key to be loaded.
+        /// </param>
+        ///
+        /// <returns> A boolean value that indicates whether the operation was successful.</returns>
         public bool Load(string password, string keyName)
         {
             return _didWallet.Load(password, keyName);
         }
 
+        /// <summary> The GetHolderInfo function is used to get the holder information of a DID.</summary>        
+        ///
+        /// <param name="GetHolderInfoByManagerParams param"> Parameters for searching for the holder information through management account address and/or chain ID.</param>
+        /// <param name="SuccessCallback successCallback"> The success callback returning the holder information.</param>
+        /// <param name="ErrorCallback errorCallback"> The error callback.</param>
+        ///
+        /// <returns> A caholderwithguardian object that contains the holder information.</returns>
         public IEnumerator GetHolderInfo(GetHolderInfoByManagerParams param, SuccessCallback<CaHolderWithGuardian> successCallback,
             ErrorCallback errorCallback)
         {
             yield return _didWallet.GetHolderInfo(param, successCallback, errorCallback);
         }
 
+        /// <summary> The GetHolderInfo function is used to get the holder information of a DID through contract view call.</summary>        
+        ///
+        /// <param name="GetHolderInfoParams param"> Parameters for searching for the holder information through caHash and/or chain ID.</param>
+        /// <param name="SuccessCallback successCallback"> The success callback returning the holder information.</param>
+        /// <param name="ErrorCallback errorCallback"> The error callback.</param>
+        ///
+        /// <returns> A IHolderInfo object that contains the holder information.</returns>
         public IEnumerator GetHolderInfoByContract(GetHolderInfoParams param, SuccessCallback<IHolderInfo> successCallback,
             ErrorCallback errorCallback)
         {
@@ -97,6 +144,12 @@ namespace Portkey.DID
             yield return _portkeySocialService.GetPhoneCountryCodeWithLocal(successCallback, errorCallback);
         }
 
+        /// <summary> The IsLoggedIn function checks to see if the user is logged in.        
+        /// &lt;para&gt;If the user is logged in, it returns true.&lt;/para&gt;
+        /// &lt;para&gt;If the user is not logged in, it returns false.&lt;/para&gt;</summary>
+        ///
+        ///
+        /// <returns> A boolean value indication if the user is logged in.</returns>
         public bool IsLoggedIn()
         {
             return _didWallet.IsLoggedIn();

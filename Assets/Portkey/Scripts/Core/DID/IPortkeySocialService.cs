2f2e3f55fe577ff33568a62feca13b0d8b9c365b
zhifenglee-portkey <lee@portkey.finance>
2023-12-08 13:43:44 +0800

    feat(portkey-social-service): added new apis for account deletion

--- Assets/Portkey/Scripts/Core/DID/IPortkeySocialService.cs
+++ Assets/Portkey/Scripts/Core/DID/IPortkeySocialService.cs
@@ -158,6 +158,18 @@ namespace Portkey.Core
         public ICountryItem locateData = null;
     }
     
+    public class AccountDeletionValidationResult
+    {
+        public bool validatedAssets;
+        public bool validatedGuardian;
+        public bool validatedDevice;
+    }
+    
+    internal class AccountDeletionParams
+    {
+        public string appleToken;
+    }
+    
     /// <summary>
     /// Social service interface for all social services to implement.
     /// </summary>
@@ -195,5 +207,17 @@ namespace Portkey.Core
         /// Get phone country code with local.
         /// </summary>
         IEnumerator GetPhoneCountryCodeWithLocal(SuccessCallback<IPhoneCountryCodeResult> successCallback, ErrorCallback errorCallback);
+        /// <summary>
+        /// An API check to see if account can be deleted. This is possible if account has only 1 login guardian that is using apple account.
+        /// </summary>
+        IEnumerator IsAccountDeletionPossible(ConnectToken connectToken, SuccessCallback<bool> successCallback, ErrorCallback errorCallback);
+        /// <summary>
+        /// Validate account deletion. User must have lesser than stipulated amount of assets, must have only 1 login guardian that is using apple account and that device is an iOS device.
+        /// </summary>
+        IEnumerator ValidateAccountDeletion(ConnectToken connectToken, SuccessCallback<AccountDeletionValidationResult> successCallback, ErrorCallback errorCallback);
+        /// <summary>
+        /// Delete account from database.
+        /// </summary>
+        IEnumerator DeleteAccount(ConnectToken connectToken, string appleToken, SuccessCallback<bool> successCallback, ErrorCallback errorCallback);
     }
 }
